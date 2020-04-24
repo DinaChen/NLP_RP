106ea9b23f930a5fbc3ba90263d9953ec54ccf7a
Dina Chen <d.chen-4@student.tudelft.nl>
2020-04-24 19:06:07 +0200

    Change the way of importing files. Add all seasons for Friends.

--- dataProcess.py
+++ dataProcess.py
@@ -1,29 +1,31 @@
 import re
 import csv
-
+import os
 
 angleBrac = '<(.|\n)*?>'        # remove <>
 roundBrac = '\(([^)]+)\)'       # remove()
 squareBrac = '\[(.|\n)*?\]'     #remove[]
-noice = '(&.*;)|(Commercial Break)|(Commercial break)|(Closing Credits)|(Opening Credits)'
+noice = '(&.*;)|(Commercial Break)|(Commercial break)|(Closing Credits)|(CLOSING CREDITS)|(Opening Credits)|(OPENING TITLES)'
 #noice = '(&nbsp;)|(&quot;)|(Commercial Break)|(Commercial break)|(Closing Credits)|(Opening Credits)|(&#146;)|(&#151;)'
 # &#151; appears in 0118, &#146; appears in 16
 
 def main():
+    path = 'C:/Users/Dina/PycharmProjects/NLP_RP/transcripts/'
+    filePaths = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.html' in file:
+                filePaths.append(os.path.join(r, file))
 
-# Generates html paths for all episodes in Season 1
-    friendS1_paths = []
-    for i in range(1, 10):
-        path ='transcripts/010'+ str(i) + ".html"
-        friendS1_paths.append(path)
-    for i in range(10, 25):
-        path ='transcripts/01'+ str(i) + ".html"
-        friendS1_paths.append(path)
 
-    friendS1 = seasonTranscript(friendS1_paths)
-    printLines(friendS1)
-    print(len(friendS1))
+    friends = seasonTranscript(filePaths)
+    printLines(friends)
+    print('#lines: ' + str(len(friends)))
+    print('#episodes: ' + str(len(filePaths)))
+    print('avg.lines per episode: ' + str(len(friends)/len(filePaths)))
+
 #bb = open('transcripts/tbbt0101.html', encoding="utf8").read()
+
 
 
 # Given transcript of 1 season in html format, out put list of lines with all irrelevant info removed, all episodes concatenate together.
@@ -71,10 +73,10 @@ def split(epi):
     toreturn = []
     for line in splitted:
 
-        j = re.sub('\n', "", line)
+        j = re.sub('\n', " ", line)
 
         if splitted.index(line) == len(splitted)-1:
-            k = re.sub('End', '', j)
+            k = re.sub('(End)|(END)', '', j)
             toreturn.append(k)
         else:
             toreturn.append(j)
